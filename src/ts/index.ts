805e15ab5bd75afcf4eebd9d60a818c1d783b136
Diego <anovaterradecristo@gmail.com>
2023-02-01 16:04:26 -0300

    feat: criado menu lateral com filtros.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,23 @@ import { Product } from "./Product";
 
 const serverUrl = "http://localhost:5000";
 
+const headers = new Headers();
+
+async function fetchApi() {
+  const req = await fetch(`${serverUrl}/products`, {
+    method: "GET",
+    headers: headers,
+    mode: "cors",
+    cache: "default",
+  })
+    .then((response) => response.json())
+    .then((data) => console.error(data));
+
+  return req;
+};
+
 function main() {
   console.log(serverUrl);
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener("DOMContentLoaded", fetchApi);
